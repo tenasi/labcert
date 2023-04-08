d949c020e7e7d6d754401471e21e64a8b74f7d6a
Jip <jip@tenasi.de>
2023-04-08 16:35:29 +0000

    Force tailwind to load paddings that are set in js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
         "./labcert/static/src/**/*.js",
         "./node_modules/flowbite/**/*.js"
     ],
+    safelist: [
+        "pl-4",
+        "pl-8",
+        "pl-12",
+        "pl-16",
+        "pl-20",
+        "pl-24",
+        "pl-28",
+        "pl-32",
+        "pl-36",
+        "pl-40",
+        "pl-44",
+        "pl-48",
+        "pl-52",
+        "pl-56",
+        "pl-60",
+        "pl-64",
+    ],
     darkMode: "class",
     theme: {
         extend: {
