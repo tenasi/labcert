990bdc04cf1110a92b639c253c7f9aec93e9aa05
Jip <jip@tenasi.de>
2023-04-09 15:51:32 +0000

    Move javascript to src folder

---
+++ labcert/static/src/settings.js
@@ -0,0 +1,25 @@
+"use strict"
+
+var pageSize = 10;
+
+(function () {
+    let isThemeInLocalStorage = "color-theme" in localStorage
+    let isBrowserInDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
+    let isThemeSetToDarkMode = localStorage.getItem("color-theme") === "dark"
+
+    let isPageSizeInLocalStorage = "page-size" in localStorage
+
+    // Set dark theme accordingly
+    if (isThemeSetToDarkMode || (!isThemeInLocalStorage && isBrowserInDarkMode)) {
+        document.documentElement.classList.add("dark")
+    } else {
+        document.documentElement.classList.remove("dark")
+    }
+
+    // Set default page size if not defined
+    if (isPageSizeInLocalStorage) {
+        pageSize = parseInt(localStorage.getItem("page-size"))
+    } else {
+        localStorage.setItem("page-size", pageSize)
+    }
+})()
