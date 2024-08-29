b4d9cc4623b5188bd8fe0322b409365d65d2d401
Jonathan Peters <jonpetersathan@gmail.com>
2024-08-29 09:09:04 +0000

    Renaming database columns

--- labcert/schema.sql
+++ labcert/schema.sql
@@ -14,15 +14,16 @@ CREATE TABLE IF NOT EXISTS certs (
     cert_id INTEGER PRIMARY KEY AUTOINCREMENT,
     cert_name TEXT NOT NULL,
     cert_level INTEGER NOT NULL,
-    cert_category TEXT NOT NULL,
+    cert_type TEXT NOT NULL,
     cert_status TEXT NOT NULL,
     cert_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     cert_serial TEXT NOT NULL,
     cert_not_valid_before TIMESTAMP NOT NULL,
     cert_not_valid_after TIMESTAMP NOT NULL,
-    subject_cn TEXT NOT NULL,
+    subject_name TEXT NOT NULL,
     subject_algorithm TEXT NOT NULL,
     subject_strength TEXT NOT NULL,
     issuer_id INTEGER,
-    issuer_name TEXT
+    issuer_name TEXT,
+    issuer_type TEXT
 );
