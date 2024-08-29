f9d53be2bbc13e01a4a390dbc5b9000c621bd932
Jonathan Peters <jonpetersathan@gmail.com>
2024-08-29 09:09:03 +0000

    Update themeToggle id

---
+++ labcert/static/dist/js/theme_toggle.js
@@ -0,0 +1,19 @@
+var themeToggle = document.getElementById('themeToggle');
+
+if (themeToggle) {
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        themeToggle.checked = true;
+    } else {
+        themeToggle.checked = false;
+    }
+
+    themeToggle.addEventListener('click', function () {
+        if (themeToggle.checked) {
+            localStorage.setItem('color-theme', 'dark');
+            document.documentElement.classList.add('dark');
+        } else {
+            localStorage.setItem('color-theme', 'light');
+            document.documentElement.classList.remove('dark');
+        }
+    });
+}
