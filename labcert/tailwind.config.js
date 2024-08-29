efdc0d0580929a76b53d996d8caa7a15235c52f3
Jonathan Peters <jonpetersathan@gmail.com>
2024-08-29 09:09:02 +0000

    Setup npm run scripts

--- labcert/tailwind.config.js
+++ labcert/tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./static/src/**/*.js",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#007bff", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
-      }
+    content: [
+        "./templates/**/*.html",
+        "./static/src/**/*.js",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#007bff", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+            }
+        },
+        fontFamily: {
+            'body': [
+                'Montserrat'
+            ],
+            'sans': [
+                'Montserrat'
+            ],
+        },
     },
-    fontFamily: {
-      'body': [
-        'Montserrat'
-      ],
-      'sans': [
-        'Montserrat'
-      ],
-    },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
